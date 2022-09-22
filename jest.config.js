65b576c1e340c7d54027a056772e801ffb590c1b
David Scholberg <recombinant.vector@gmail.com>
2022-09-22 14:00:47 -0400

    cleaned up jest config file

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // A map from regular expressions to paths to transformers
+  // see https://jestjs.io/docs/ecmascript-modules
+  transform: {},
+};
