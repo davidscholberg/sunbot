5195d69995ff981d8c7dc554d14f116498ba3744
David Scholberg <recombinant.vector@gmail.com>
2022-10-02 18:51:27 -0400

    more wip changes

--- src/bot/command_map.js
+++ src/bot/command_map.js
@@ -2,30 +2,47 @@ import axios from 'axios';
 import convert from 'convert-units';
 
 /* eslint-disable import/extensions */
-import makeBoopCommand from '../commands/boop.js';
-import makeHelloCommand from '../commands/hello.js';
+import boopCommand from '../commands/boop.js';
+import helloCommand from '../commands/hello.js';
 import makeTenor from '../lib/tenor.js';
 import makeWeatherbit from '../lib/weatherbit.js';
-import makeWeatherCommand from '../commands/weather.js';
 import makeYoutubeSearch from '../lib/youtube.js';
-import makeYoutubeCommand from '../commands/youtube.js';
+import weatherCommand from '../commands/weather.js';
+import youtubeCommand from '../commands/youtube.js';
 /* eslint-enable import/extensions */
 
-export default function makeGetCommandMap(config) {
-  const commandList = [
-    makeBoopCommand(makeTenor(config, axios)),
-    makeHelloCommand(),
-    makeWeatherCommand(makeWeatherbit(config, axios), convert),
-    makeYoutubeCommand(makeYoutubeSearch(config, axios)),
-  ];
+function addCommandMapEntry(commandMap, command, execute) {
+  const commandData = command.makeCommandData();
+  // eslint-disable-next-line no-param-reassign
+  commandMap[commandData.name] = {
+    commandData,
+    execute,
+  };
+}
+
+export default function makeGetCommandMap(config, respond) {
   let commandMap = null;
   return () => {
     if (commandMap !== null) {
       return commandMap;
     }
     commandMap = {};
-    commandList.forEach((command) => {
-      commandMap[command.commandData.name] = command;
+    addCommandMapEntry(commandMap, boopCommand, async (data) => {
+      const execute = boopCommand.makeExecute(respond, makeTenor(config, axios));
+      return execute(data);
+    });
+    addCommandMapEntry(commandMap, helloCommand, async (data) => {
+      const execute = helloCommand.makeExecute(respond);
+      return execute(data);
+    });
+    // TODO: add talk command
+    addCommandMapEntry(commandMap, weatherCommand, async (data) => {
+      const execute = weatherCommand.makeExecute(respond, makeWeatherbit(config, axios), convert);
+      return execute(data);
+    });
+    addCommandMapEntry(commandMap, youtubeCommand, async (data) => {
+      const execute = youtubeCommand.makeExecute(respond, makeYoutubeSearch(config, axios));
+      return execute(data);
     });
     return commandMap;
   };
--- src/commands/talk.js
+++ src/commands/talk.js
@@ -1,37 +1,28 @@
-export default function makeTalkCommand(textToSpeech, mediaConvert, FormData) {
-  return {
-    commandData: {
-      name: 'talk',
-      type: 1,
-      description: 'make the bot talk!',
-      options: [
-        {
-          name: 'sentence',
-          type: 3,
-          description: 'words to make the bot say',
-          required: true,
-        },
-      ],
-    },
-    respond: async (data, response) => {
-      const sentence = data.options[0].value.trim();
-      if (sentence.length === 0) {
-        throw new Error('sentence parameter is empty');
-      }
-      const ttsResponse = await textToSpeech.synthesize({
-        text: sentence,
-        accept: 'audio/ogg;codecs=opus',
-        voice: 'en-US_MichaelV3Voice',
-      });
-      const videoData = await mediaConvert.audioToMp4(ttsResponse.result);
-      const form = FormData();
-      form.append('video', videoData);
-      form.append('json_data', JSON.stringify({
-
-      }));
-      response.multiValueHeaders = form.getHeaders();
-      response.body = form.getBuffer();
-      return response;
-    },
-  };
-}
+export default {
+  makeCommandData: () => ({
+    name: 'talk',
+    type: 1,
+    description: 'make the bot talk!',
+    options: [
+      {
+        name: 'sentence',
+        type: 3,
+        description: 'words to make the bot say',
+        required: true,
+      },
+    ],
+  }),
+  makeExecute: (respond, textToSpeech, mediaConvert) => async (data) => {
+    const sentence = data.options[0].value.trim();
+    if (sentence.length === 0) {
+      throw new Error('sentence parameter is empty');
+    }
+    const ttsResponse = await textToSpeech.synthesize({
+      text: sentence,
+      accept: 'audio/ogg;codecs=opus',
+      voice: 'en-US_MichaelV3Voice',
+    });
+    const videoData = await mediaConvert.audioToMp4(ttsResponse.result);
+    return respond.withAttachment('sunbot has a message:', videoData);
+  },
+};
